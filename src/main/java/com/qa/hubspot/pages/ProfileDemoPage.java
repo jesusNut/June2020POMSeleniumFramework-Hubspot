b25c78a87fdc023e31c9613f611b900e45377e49
Abhishek Bhardwaj <70820424+jesusNut@users.noreply.github.com>
2020-09-08 00:02:45 +0530

    Update ProfileDemoPage.java

--- src/main/java/com/qa/hubspot/pages/ProfileDemoPage.java
+++ src/main/java/com/qa/hubspot/pages/ProfileDemoPage.java
@@ -5,9 +5,10 @@ import com.qa.hubspot.base.BasePage;
 public class ProfileDemoPage extends BasePage {
 
 	public String provideAStringProfile() {
-
+		
+                 System.out.println("i am the xtra code :)");
 		return "jetha laal";
-                System.out.println("i am the xtra code :)");
+                
 	}
 	
 }
