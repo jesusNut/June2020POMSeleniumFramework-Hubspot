ebac210c257c237f2b5b4622a29bd179043a8d8e
Abhishek Bhardwaj <abhishekbhardwaj.sliet@gmail.com>
2020-09-07 23:06:35 +0530

    added profileDemo page

---
+++ src/main/java/com/qa/hubspot/pages/ProfileDemoPage.java
@@ -0,0 +1,13 @@
+package com.qa.hubspot.pages;
+
+import com.qa.hubspot.base.BasePage;
+
+public class ProfileDemoPage extends BasePage {
+
+	public String provideAStringProfile() {
+
+		return "jetha laal";
+
+	}
+	
+}
