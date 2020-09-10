4f03667ec989d2c113d5829fd3993dc709dbc6bf
Abhishek Bhardwaj <70820424+jesusNut@users.noreply.github.com>
2020-09-11 01:48:43 +0530

    Update Constants.java

--- src/main/java/com/qa/hubspot/utils/Constants.java
+++ src/main/java/com/qa/hubspot/utils/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 
 	public static final String HOME_PAGE_TITLE = "Account Setup | HubSpot";
 	public static final String HOME_PAGE_HEADER = "Setup Guide";
-	public static final String LOGIN_PAGE_TITLE = "HubSpot Login";
+	public static final String LOGIN_PAGE_TITLE = "CubSpot Login";
 	public static final String CONTACTS_PAGE_TITLE = "Contacts";
 	public static final String CONTACTS_PAGE_HEADER = "Contacts";
 	public static final String DEALS_PAGE_TITLE = "Deals";
